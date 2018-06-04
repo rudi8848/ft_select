83375914fcb890a78a51607f445aa336edc93c24
rudi8848 <anna.v8848@gmail.com>
2018-06-04 14:09:38 +0300

    1) SIGTSTP works after 2 presses 2) SIGCONT - cursor starts from 1-st but it's olso outputs at last chosen 3)SIGINT, SIGWINCH - good

--- signals.c
+++ signals.c
@@ -12,36 +12,38 @@
 
 #include "ft_select.h"
 
-void		ft_stp_handler(int signum)
+void		ft_tstp_handler(int signum)
 {
-	;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_attr.def_settings);
+	signal(signum, SIG_DFL);
 }
 
 void		ft_int_handler(int signum)
 {
-	;
+	ft_exit();
 }
 
 void		ft_winch_handler(int signum)
 {
 	ft_get_winsize();
 	ft_putstr_fd(CLEAR, STDERR_FILENO);
-	ft_print_forward(g_attr.args);
 }
 
-void		ft_count_handler(int signum)
+void		ft_cont_handler(int signum)
 {
-	;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_attr.cur_settings);
+	//ft_print_forward(g_attr.args);
+	ft_processing();
 }
 
 void		ft_set_signals(void)
 {
-	//	signal(SIGSTP, ft_stp_handler);	
+		signal(SIGTSTP, ft_tstp_handler);
+		signal(SIGCONT, ft_cont_handler);	
 		signal(SIGINT, ft_int_handler);	
 		signal(SIGABRT, ft_int_handler);	
-		signal(SIGQUIT, ft_int_handler);	
-	//	signal(SIGCOUNT, ft_count_handler);	
-		signal(SIGSTOP, ft_stp_handler);	
+		signal(SIGQUIT, ft_int_handler);
+		//signal(SIGSTOP, ft_stp_handler);	
 		signal(SIGWINCH, ft_winch_handler);	
 		signal(SIGKILL, ft_int_handler);	
 }
